1efde5b9bf46e78aaa91e2543247f66cc0085ce7
mhutch14 <hutchimw@miamioh.edu>
2017-05-06 23:16:37 -0400

    Add files via upload

---
+++ I2C_Communication_Master.ino
@@ -0,0 +1,29 @@
+//i2c Slave Code
+#include <Wire.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Wire.begin();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while(Serial.available()){
+  char c = Serial.read();
+  if(c == 'H'){
+    Wire.beginTransmission(5);
+    Wire.write('H');
+    Wire.endTransmission();
+  }else if(c == 'L'){
+    Wire.beginTransmission(5);
+    Wire.write('L');
+    Wire.endTransmission();
+  }
+  
+  }
+}
